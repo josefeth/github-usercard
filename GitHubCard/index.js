548f4c4da0bc320a7bcd819dd541f1507cccec9d
Josef (Joey) Etheridge <56942351+josefeth@users.noreply.github.com>
2020-01-16 14:07:34 -0700

    done and working on mvp

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,3 +1,72 @@
+
+const cards = document.querySelector('.cards');
+
+axios.get("https://api.github.com/users/josefeth")
+.then(response => {
+  cards.append(gitCard(response.data))
+  });
+
+const followersArray = ['bigknell','tetondan','dustinmyers','justsml','luishrd'];
+followersArray.forEach(item =>{axios.get(`https://api.github.com/users/${item}`)
+.then(response => {
+  cards.append(gitCard(response.data))
+ });
+})
+
+{/* <div class="card">
+  <img src={image url of user} />
+  <div class="card-info">
+    <h3 class="name">{users name}</h3>
+    <p class="username">{users user name}</p>
+    <p>Location: {users location}</p>
+    <p>Profile:  
+      <a href={address to users github page}>{address to users github page}</a>
+    </p>
+    <p>Followers: {users followers count}</p>
+    <p>Following: {users following count}</p>
+    <p>Bio: {users bio}</p>
+  </div>
+</div> */}
+
+function gitCard(data){
+
+  const newCard = document.createElement('div')
+  const img = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const h3 = document.createElement('h3')
+  const p1 = document.createElement('p')
+  const p2 = document.createElement('p')
+  const p3 = document.createElement('p')
+  const a = document.createElement('a')
+  const p4 = document.createElement('p')
+  const p5 = document.createElement('p')
+  const p6 = document.createElement('p')
+        
+
+        newCard.append(img);
+        newCard.append(cardInfo);
+        cardInfo.append(h3, p1, p2, p3, p4, p5,p6);
+        p3.append(a);
+
+        newCard.classList.add('card');
+        cardInfo.classList.add('card-info');
+        h3.classList.add('name');
+        p1.classList.add('username');
+
+
+        img.src = data.avatar_url;
+        h3.textContent = `${data.name}`;
+        p1.textContent = `${data.login}`;
+        p2.textContent = `Location: ${data.location}`;
+        a.textContent = `Profile: ${data.html_url}`;
+        a.href = `${data.html_url}`;
+        p4.textContent = `Followers: ${data.followers}`;
+        p5.textContent = `Following: ${data.following}`;
+        p6.textContent = `Bio: ${data.bio}`;
+
+      return newCard;
+}
+
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
@@ -6,7 +75,6 @@
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
-
    Skip to Step 3.
 */
 
@@ -24,11 +92,10 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
-
 <div class="card">
   <img src={image url of user} />
   <div class="card-info">
@@ -43,7 +110,6 @@ const followersArray = [];
     <p>Bio: {users bio}</p>
   </div>
 </div>
-
 */
 
 /* List of LS Instructors Github username's: 
@@ -53,3 +119,4 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
